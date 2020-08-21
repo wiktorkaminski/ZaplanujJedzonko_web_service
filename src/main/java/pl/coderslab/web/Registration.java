f8fb03ebe9bd47eea0a47a7edaa62a140849a657
wiktorkaminski <wiktor.kaminski@mixbox.pl>
2020-08-21 23:10:51 +0200

    registration doGet + jsp

--- src/main/java/pl/coderslab/web/Registration.java
+++ src/main/java/pl/coderslab/web/Registration.java
@@ -14,6 +14,6 @@ public class Registration extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        getServletContext().getRequestDispatcher("/registration.jsp").forward(request, response);
     }
 }
