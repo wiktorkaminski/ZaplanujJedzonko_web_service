36c6304c215a10ec1f5ca5e26096cdb8063927f8
MarcinKubik <kubikmmarcin@gmail.com>
2020-08-21 23:12:34 +0200

    ContactAbout

---
+++ src/main/java/pl/coderslab/web/AboutAppServlet.java
@@ -0,0 +1,20 @@
+package pl.coderslab.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "AboutAppServlet", value = "/about")
+public class AboutAppServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //getServletContext().getRequestDispatcher("/home#about").forward(request, response);
+        response.sendRedirect("/home#about");
+    }
+}
---
+++ src/main/java/pl/coderslab/web/ContactServlet.java
@@ -0,0 +1,20 @@
+package pl.coderslab.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ContactServlet", value = "/contact")
+public class ContactServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //getServletContext().getRequestDispatcher("/home#contact").forward(request, response);
+        response.sendRedirect("/home#contact");
+    }
+}
