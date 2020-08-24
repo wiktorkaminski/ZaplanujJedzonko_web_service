038e07566f6926a26f9553875e64f09e42910806
wiktorkaminski <wiktor.kaminski@mixbox.pl>
2020-08-24 14:27:05 +0200

    fixed HomeServlet

--- src/main/java/pl/coderslab/web/HomeServlet.java
+++ src/main/java/pl/coderslab/web/HomeServlet.java
@@ -1,21 +1,13 @@
 package pl.coderslab.web;
 
-import pl.coderslab.dao.AdminDao;
 import pl.coderslab.dao.BookDao;
-<<<<<<< HEAD
-import pl.coderslab.model.Admin;
-=======
-import pl.coderslab.dao.PlanDao;
->>>>>>> 5_strona_główna_aplikacji
 import pl.coderslab.model.Book;
-import pl.coderslab.model.RecentPlanDetail;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -30,9 +22,6 @@ public class HomeServlet extends HttpServlet {
         List<Book> books = bookDao.findAll();
         System.out.println(books);
 
-        HttpSession session = request.getSession();
-        session.setAttribute("adminId", 1);
-        
         getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
     }
 }
