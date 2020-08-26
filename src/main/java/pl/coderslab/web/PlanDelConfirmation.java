0dd24f44be3251f5e8973e77d44cdf858a64767e
wiktorkaminski <wiktor.kaminski@mixbox.pl>
2020-08-26 20:48:18 +0200

    del confirmation

--- src/main/java/pl/coderslab/web/PlanDelConfirmation.java
+++ src/main/java/pl/coderslab/web/PlanDelConfirmation.java
@@ -7,13 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "PlanDeleteConfirmation")
+@WebServlet(name = "PlanDeleteConfirmation", value = "/app/plan/del/recipe-del-confirmation")
 public class PlanDelConfirmation extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        getServletContext().getRequestDispatcher("/app/recipe-del-confirmation.jsp").forward(request, response);
     }
 }
