8ea503912b80fe3d468c7ed94050ee6509e6dab0
MarcinKubik <kubikmmarcin@gmail.com>
2020-08-27 22:10:05 +0200

    17FirstCommit

---
+++ src/main/java/pl/coderslab/web/ScheduleDelete.java
@@ -0,0 +1,23 @@
+package pl.coderslab.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "ScheduleDelete", value = "/app/plan/delete")
+public class ScheduleDelete extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String idToDelete = request.getParameter("id");
+        HttpSession session = request.getSession();
+        session.setAttribute("idToDelete", Integer.parseInt(idToDelete));
+        getServletContext().getRequestDispatcher("/app/delete-recipe.jsp").forward(request, response);
+    }
+}
